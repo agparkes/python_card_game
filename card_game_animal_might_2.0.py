412696099bb53d6b6f24ccbafad59ca098b12a95
agparkes <agparkes413@gmail.com>
2019-03-06 16:43:30 -0500

    initial commit

---
+++ card_game_animal_might_2.0.py
@@ -0,0 +1,21 @@
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit  = suit
+        
+    def __repr__(self):
+#        return "{} of {}".format(self.value, self.suit) #format less than python 3.6
+        return f"{self.value} of shield {self.suit}" 
+
+class Deck:
+    def __init__(self):
+        suits      = ["Lion", "Bear", "Wolf", "Gorilla"]
+        values     = ["Emerald", "Diamond", "Ruby", "Sapphire", "Star", "Moon", "Fire", "Wind", "Earth", "Thunder", "Lightling", "Safe"]
+        self.cards = []        
+        for suit in suits:
+            for value in values:
+                print(Card(value, suit))
+                
+        print(self.cards)
+        
+d = Deck()
