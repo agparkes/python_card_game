fe3f4e195ef37fd98155825a6062278645cd4fa4
agparkes <agparkes413@gmail.com>
2019-03-07 08:29:45 -0500

    add instance methods to the two class, card and deck. Import shuffle from random to shuffle cards. To handle error message, add custom ValueError message.

--- card_game_animal_might_2.0.py
+++ card_game_animal_might_2.0.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 class Card:
     def __init__(self, value, suit):
         self.value = value
@@ -13,6 +15,42 @@ class Deck:
         values     = ["Emerald", "Diamond", "Ruby", "Sapphire", "Star", "Moon", "Fire", "Wind", "Earth", "Thunder", "Lightling", "Safe"]
         self.cards = [Card(value, suit) for suit in suits for value in values] 
         
-        print(self.cards)
+    def __repr__(self):
+        return f"Deck of {self.count()} cards"
+
+    def count(self):
+        return len(self.cards)
+
+    def _deal(self, num):
+        count = self.count()
+        actual = min([count, num])
+        if count == 0:
+            raise ValueError ("All cards have been dealt")
+        cards = self.cards[-actual:]
+        self.cards = self.cards[:-actual]
+        return cards
+        print(f"Going to remove {actual} cards")
+        
+    def deal_card(self):
+        return self._deal(1)[0] 
+
+    def deal_hand(self, hand_size):
+        return self._deal(hand_size)
+
+    def shuffle_deck(self):
+        if self.count() < 48:
+            raise ValueError("Only full deck can be shuffled")
+        shuffle(self.cards)
+        return self #best practice to return self, a conventional thing to do
         
 d = Deck()
+d.shuffle_deck()
+print(d._deal(3))
+print(f"There are {d.count()} cards remaining")
+#print(d._deal(48))
+#d.shuffle_deck()
+card = d.deal_card()
+print(card)
+hand = d.deal_hand(5)
+print(hand)
+print(d.cards)
